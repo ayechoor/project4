1cceeba020d5f4c5c068dbb1a5d2828a4ef03c70
Aparna Yechoor <ayechoor@umich.edu>
2016-12-03 18:19:50 -0500

    added collision detection

--- pythongame.py
+++ pythongame.py
@@ -2,6 +2,7 @@ import pygame
 from pygame import *
 from pygame.sprite import *
 import random
+import sys
 pygame.init()
 
 white = (255,255,255)
@@ -9,14 +10,18 @@ black = (0,0,0)
 red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
+purple = (230,0, 240)
+yellow = (255, 255, 0)
+gray = (70,70,70)
 
 speed = pygame.time.Clock()
+gameDisplay = pygame.display.set_mode((500,500))
 
 class Player(pygame.sprite.Sprite): 
-	def __init__(self):          
+	def __init__(self, color):          
 		pygame.sprite.Sprite.__init__(self) 
 		self.image = pygame.Surface([24, 24])
-		self.image.fill(green)
+		self.image.fill(color)
 		self.rect=self.image.get_rect()
 		self.x_delta=0	
 		self.y_delta=0
@@ -38,40 +43,67 @@ class Barrier(pygame.sprite.Sprite):
 		self.image.fill(red)
 		self.rect=self.image.get_rect().move(a,b)
 
+class Flag(pygame.sprite.Sprite):
+	def __init__(self, color):          
+		pygame.sprite.Sprite.__init__(self) 
+		self.image = pygame.Surface([24, 24])
+		self.image.fill(black)
+		self.rect=self.image.get_rect()
+		pygame.draw.circle(self.image, color, (12,12), 12, 0)
+
+def checkCollision(sprite1, sprite2):
+	col = pygame.sprite.spritecollide(sprite1, sprite2, True)
+	if col!=[]:
+		font = pygame.font.Font(None, 100)
+		text=font.render("GAME OVER", 1, yellow)
+		gameDisplay.blit(text, (40, 200))
+		pygame.display.update()
+		print ("You lost.")
+		pygame.time.delay(4000)
+		sys.exit()
+
+
+b1=Barrier(300, 50, 0, 60)
+b2=Barrier(50, 400, 100, 300)
+b3=Barrier(50, 400, 300, 300)
+b4=Barrier(300, 50, 200, 175)
+p=Player(white)
+f=Flag(blue)
+
+bar_list=Group(b1, b2, b3, b4)
+sprite_list=Group(p, f)
+all_list=Group(bar_list, sprite_list)
 
 def main():
-
-	gameDisplay = pygame.display.set_mode((500,500))
-	
-	
-	p1=Player()
-	b1=Barrier(300, 50, 0, 50)
-	b2=Barrier(50, 400, 100, 300)
-	b3=Barrier(50, 400, 300, 300)
-	b4=Barrier(300, 50, 200, 150)
-
-	sprite_list=Group(p1, b1, b2, b3, b4)
 
 	gameExit = False
 	while not gameExit:
 		gameDisplay.fill(black)
-		sprite_list.draw(gameDisplay)
+		all_list.draw(gameDisplay)
 		
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				gameExit = True
+
+		if p.rect.colliderect(f.rect):
+			sprite_list.remove(f)
+			#f.update()
+			#sprite_list.add(f)
 			
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_LEFT:
-				p1.left()					
+				p.left()					
 			if event.key == pygame.K_RIGHT:
-				p1.right()
+				p.right()
 			if event.key == pygame.K_UP:
-				p1.up()
+				p.up()
 			if event.key == pygame.K_DOWN:
-				p1.down()
-		
-			p1.update()
+				p.down()
+			p.update()
+
+		sprite_list.update()
+		checkCollision(p, bar_list)
+
 		pygame.display.update()
 		speed.tick(27)	
 				
