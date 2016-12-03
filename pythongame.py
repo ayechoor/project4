8620e81290ec03d40e0450955fa5638a88be3704
Aparna Yechoor <ayechoor@umich.edu>
2016-12-03 17:40:46 -0500

    added barrier class

--- pythongame.py
+++ pythongame.py
@@ -1,8 +1,8 @@
 import pygame
 from pygame import *
+from pygame.sprite import *
+import random
 pygame.init()
-
-gameDisplay = pygame.display.set_mode((500,500))
 
 white = (255,255,255)
 black = (0,0,0)
@@ -10,45 +10,71 @@ red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
 
-class Snake(pygame.sprite.Sprite): 
+speed = pygame.time.Clock()
 
+class Player(pygame.sprite.Sprite): 
 	def __init__(self):          
 		pygame.sprite.Sprite.__init__(self) 
-		self.image = pygame.surface.Surface([20, 20])
-		x_pos = 0
-		y_pos = 0
-		x_delta=0
-		y_delta=0 
-		self.rect = rect(x_pos,y_pos, 20, 20)
+		self.image = pygame.Surface([24, 24])
+		self.image.fill(green)
+		self.rect=self.image.get_rect()
+		self.x_delta=0	
+		self.y_delta=0
+	def down(self):
+		self.y_delta += 10
+	def up(self):
+		self.y_delta -= 10
+	def left(self):
+		self.x_delta -= 10
+	def right(self):
+		self.x_delta += 10
 	def update(self):
-		self.inflate_ip(10,10)
-		if event.key == pygame.K_LEFT:
-			x_delta -= 10
-		if event.key == pygame.K_RIGHT:
-			x_delta += 10
-		if event.key == pygame.K_UP:
-			y_delta -= 10
-		if event.key == pygame.K_DOWN:
-			y_delta += 10
-		self.rect = rect(x_pos,y_pos)	
+		self.rect = self.image.get_rect().move(self.x_delta, self.y_delta)
+
+class Barrier(pygame.sprite.Sprite):
+	def __init__(self,width,height,a,b):          
+		pygame.sprite.Sprite.__init__(self) 
+		self.image = pygame.Surface([width, height])
+		self.image.fill(red)
+		self.rect=self.image.get_rect().move(a,b)
+
 
 def main():
 
-	speed = pygame.time.Clock()
+	gameDisplay = pygame.display.set_mode((500,500))
+	
+	
+	p1=Player()
+	b1=Barrier(300, 50, 0, 50)
+	b2=Barrier(50, 400, 100, 300)
+	b3=Barrier(50, 400, 300, 300)
+	b4=Barrier(300, 50, 200, 150)
 
-	nag=Snake()
+	sprite_list=Group(p1, b1, b2, b3, b4)
 
 	gameExit = False
 	while not gameExit:
 		gameDisplay.fill(black)
-
+		sprite_list.draw(gameDisplay)
+		
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				gameExit = True
-			if event.type == pygame.KEYDOWN:
-				nag.update()
-		pygame.display.update()	
-		speed.tick(10)
+			
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_LEFT:
+				p1.left()					
+			if event.key == pygame.K_RIGHT:
+				p1.right()
+			if event.key == pygame.K_UP:
+				p1.up()
+			if event.key == pygame.K_DOWN:
+				p1.down()
+		
+			p1.update()
+		pygame.display.update()
+		speed.tick(27)	
+				
 
 main()
 
